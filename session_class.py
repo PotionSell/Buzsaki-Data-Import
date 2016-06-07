4efb4d0fc3032442906076290942c6018e47ca0b
PotionSell <b3cousins@aol.com>
2016-06-07 12:11:47 -0400

    Add files via upload

--- session_class.py
+++ session_class.py
@@ -95,33 +95,61 @@ class Session():
         
         #LED position data
         self.LED_posData = numpy.genfromtxt(self.sessionName+'.whl.csv', delimiter = ',')
+        self.LED_posData = numpy.around(.01 * self.LED_posData, 10)
         
-        
-        #time data for each electrode
-        
-        #self.res_files = numpy.array([])
+        #cluster timestamp data from each shank
         self.res_files = []
         for i in os.listdir(os.getcwd()):                   #get each .res file
-            if 'res' in i and i.index('res') == len(sessionName)+1:
+            if '.res' in i and i.index('.res') == len(sessionName):
                 self.res_files.append(i)
-        print self.res_files
-        sort_nicely( self.res_files)
-        print self.res_files
+        sort_nicely(self.res_files)
+        self.cluster_times_list = []                        #create a list of data arrays from files
+        for x in range(len(self.res_files)):
+            array = numpy.genfromtxt(self.res_files[x])
+            array = numpy.around(1./20000. * array, 10)     #converts to seconds
+            self.cluster_times_list.append(array)
         
-        self.recording_timestamps = numpy.array([])
-        for i in self.res_files:
-#            print numpy.genfromtxt(i)
-            array = numpy.genfromtxt(i)
-            print array.shape
-            array = numpy.transpose(array.reshape((-1, 1)))             #make the array 2D, then transpose it
-            print array.shape
-            self.recording_timestamps = numpy.append(self.recording_timestamps, numpy.genfromtxt(i))
-        print self.recording_timestamps
-        print self.recording_timestamps.shape
-#        for i in self.recording_timestamps:
+        #cluster number data from each shank
+        self.clu_files = []
+        for i in os.listdir(os.getcwd()):                   #get each .clu file
+            if '.clu' in i and i.index('.clu') == len(sessionName):
+                self.clu_files.append(i)
+        sort_nicely(self.clu_files)
+        self.cluster_num_list = []
+        for x in range(len(self.clu_files)):
+            array = numpy.genfromtxt(self.clu_files[x])
+            array = array[1:].astype(int)                   #trim the first line since it's not data
+            self.cluster_num_list.append(array)
+            
+#        arrays = tuple(arrays)
+#        self.cluster_times_list = numpy.column_stack(arrays)
+#        print self.cluster_times_list
+            
+#            array = numpy.transpose(array.reshape((-1, 1)))             #make the array 2D, then transpose it
+#            print array.shape
+#            self.cluster_times_list[x] = array
+##            self.cluster_times_list = numpy.append(self.cluster_times_list, numpy.genfromtxt(i))
+#        print self.cluster_times_list
+#        print self.cluster_times_list.shape
+        
+#        for i in self.cluster_times_list:
 #            for x in range(0, i.shape[0]):
 #                i[x] = i[x] * 100
-#        print self.recording_timestamps
+#        print self.cluster_times_list
+#        for i in self.res_files:
+##            print numpy.genfromtxt(i)
+#            array = numpy.genfromtxt(i)
+#            print array.shape
+#            array = numpy.transpose(array.reshape((-1, 1)))             #make the array 2D, then transpose it
+#            print array.shape
+#            self.cluster_times_list = numpy.append(self.cluster_times_list, array)
+#        print self.cluster_times_list
+#        print self.cluster_times_list.shape
+
+##        for i in self.cluster_times_list:
+##            for x in range(0, i.shape[0]):
+##                i[x] = i[x] * 100
+##        print self.cluster_times_list
         
         os.chdir(cwd)
         
@@ -166,26 +194,26 @@ class Session():
     def make_posDict(self):
         '''
         Makes a python dictionary of the session position timestamps and position data.
-        Returns a list of dictionaries, one corresponding to each LED, and one corresponding to calculated real position
+        Returns three dictionaries, one corresponding to each LED, and one corresponding to calculated real position
         '''
     
         pos_timestamps = self.make_pos_timestamps()
         
         dict_1 = {}                                                         #dict for LED1 positions
         #####*************
-        for i in range (    int(self.LED_posData.shape[0]   *.2)):     #here I assume that there are equal #s of x and y positions
-            if self.LED_posData[i,0] != -1 and self.LED_posData[i,1] != -1:         #trim out the invalid position values (-1)
+        for i in range (    int(self.LED_posData.shape[0]   *1)):     #here I assume that there are equal #s of x and y positions
+            if self.LED_posData[i,0] >= 0 and self.LED_posData[i,.1] >= 0:         #trim out the invalid position values (-1)
                 dict_1.update({pos_timestamps[i]: self.LED_posData[i,:2]})          #unsorted dict
         dict_1 = OrderedDict(sorted(dict_1.items(), key=lambda t: t[0]))    #sorted dict; could I combine these two steps?
         
         dict_2 = {}                                                         #dict for LED2 positions
         #####************************************************************
-        for i in range (    int(self.LED_posData.shape[0]   *.2)):
-            if self.LED_posData[i,2] != -1 and self.LED_posData[i,3] != -1:
+        for i in range (    int(self.LED_posData.shape[0]   *1)):
+            if self.LED_posData[i,2] >= 0 and self.LED_posData[i,3] >= 0:
                 dict_2.update({pos_timestamps[i]: self.LED_posData[i,2:]})          #unsorted dict
         dict_2 = OrderedDict(sorted(dict_2.items(), key=lambda t: t[0]))    #sorted dict
         
-        print '***Calculating intermediate position - this will take some time***'
+        print '***Calculating intermediate rat position - this will take some time***'
         dict_3 = {}
         if len(dict_1.keys()) <= len(dict_2.keys()):        #use the LED data with the fewest valid positions as reference to ensure proper position calculations
             reference_dict = dict_1
@@ -196,8 +224,23 @@ class Session():
             dict_3.update({pos_timestamps[i]: [round((round(dict_1[pos_timestamps[i]][0], 10) + round(dict_2[pos_timestamps[i]][0], 10))/2, 10), round((round(dict_1[pos_timestamps[i]][1], 10) + round(dict_2[pos_timestamps[i]][1], 10))/2, 10)]})
         dict_3 = OrderedDict(sorted(dict_3.items(), key=lambda t: t[0]))
         
-        posDict = [dict_1,dict_2,dict_3]
-        return posDict
+        print '***Done calculating position***'
+        return (dict_1,dict_2,dict_3)
+        
+        
+    def get_unit_times(self, x):
+        
+        times = {}
+#        for x in range(len(self.cluster_num_list)):
+        a = self.cluster_num_list[x]
+        u = numpy.unique(a)                                         #list of different cluster numbers for current shank
+        indices = [numpy.argwhere(i==a) for i in numpy.unique(a)]         #list of indices of each cluster firing in the .clu data
+        for y in range(len(u)):                                     #for each cluster in current shank
+            shank_cluster_indices = indices[y]
+            shank_times = self.cluster_times_list[x]
+            t = shank_times[shank_cluster_indices]                  #find only the times that correspond to the current cluster firing
+            times.update({u[y]: numpy.squeeze(t, axis=(1,))})       #numpy.squeeze changes from 2D to 1D
+        return (u, times)
         
         
         
@@ -206,10 +249,13 @@ class Session():
         Removes any files generated by the program for python compatibility.
         '''
         
+        print 'Cleaning up files'
+        
         os.chdir(self.sessionDir)
         
         os.remove(self.sessionName+'.whl.csv')
         
+        print 'Done cleaning files'
         
         
 def write_nwb(sessionName):
@@ -243,7 +289,7 @@ def write_nwb(sessionName):
     #create .nwb groups
     head_position = f.make_group('<module>', 'head_position')
     Position = head_position.make_group('Position', attrs= {'source': 'Data as reported in experiment files', 
-                    'neurodata_type': 'Interface', 'description': 'Position data, whether along the x, xy or xyz axis.'})
+                    'neurodata_type': 'Interface', 'help': 'Position data, whether along the x, xy or xyz axis.'})
 
     
     #### write LED1 position data ####
@@ -256,7 +302,7 @@ def write_nwb(sessionName):
     pos_timestamps_1 = posDict[0].keys()
     pos_num_samples_1 = len(pos_timestamps_1)
     
-    LED1.set_dataset('data', pos_data_1, attrs= {'unit': 'Centimeters', 'conversion': '100', 'resolution': '0.1'})
+    LED1.set_dataset('data', pos_data_1, attrs= {'unit': 'Meters', 'conversion': '1', 'resolution': '0.001'})
             #I changed the conversion/resolution due to using CM instead of M; make sure they are correct
     LED1.set_dataset('timestamps', pos_timestamps_1, attrs= {'unit': 'Seconds', 'interval': '1'})
     LED1.set_dataset('num_samples', pos_num_samples_1)
@@ -272,7 +318,7 @@ def write_nwb(sessionName):
     pos_timestamps_2 = posDict[1].keys()
     pos_num_samples_2 = len(pos_timestamps_2)
     
-    LED2.set_dataset('data', pos_data_2, attrs= {'unit': 'Centimeters', 'conversion': '100', 'resolution': '0.1'})
+    LED2.set_dataset('data', pos_data_2, attrs= {'unit': 'Meters', 'conversion': '1', 'resolution': '0.001'})
             #I changed the conversion/resolution due to using CM instead of M; make sure they are correct
     LED2.set_dataset('timestamps', pos_timestamps_2, attrs= {'unit': 'Seconds', 'interval': '1'})
     LED2.set_dataset('num_samples', pos_num_samples_2)
@@ -288,24 +334,34 @@ def write_nwb(sessionName):
     pos_timestamps_real = posDict[2].keys()
     pos_num_samples_real = len(pos_timestamps_real)
     
-    position.set_dataset('data', pos_data_real, attrs= {'unit': 'Centimeters', 'conversion': '100', 'resolution': '0.1'})
-            #I changed the conversion/resolution due to using CM instead of M; make sure they are correct
+    position.set_dataset('data', pos_data_real, attrs= {'unit': 'Meters', 'conversion': '1', 'resolution': '0.001'})
+            #the exemplar files have the real position values around 10^1 or 10^2 degrees of magnitude, in meters -> I think this is an error on their part
     position.set_dataset('timestamps', pos_timestamps_real, attrs= {'unit': 'Seconds', 'interval': '1'})
     position.set_dataset('num_samples', pos_num_samples_real)
     position.set_dataset('reference_frame', 'Top of room, as seen from camera')
 
-    #testing:
-#    pos_dict = session.make_posDict()
-#    print len(pos_dict[0])
-#    print OrderedDict(pos_dict[0].items()[2000:2565-301])
-#    print len (pos_dict[1])
-#    print OrderedDict(pos_dict[1].items()[1800:2565-301])
-
-    
-    
+    #### create nwb groups/datasets for each electrode (eg. shank_0, shank_1, etc.) ####
+    for x in range(len(session.cluster_times_list)):
+        name = ('shank_', str(x))
+        shank = f.make_group('<module>', ''.join(name))
+        clustering = shank.make_group('Clustering', attrs= {'description': 'Clustered spike data, whether from automatic \
+clustering tools (eg, klustakwik) or as a result of manual sorting', 'source': 'FeatureExtraction interface, this module'})
+        clustering.set_dataset('times', session.cluster_times_list[x])
+        clustering.set_dataset('num', session.cluster_num_list[x])
+        clustering.set_dataset('description', 'Cluster #0 is electrical noise, #1 is multi-unit/unsorted, and higher numbers are unit clusters')
+        
+        cluster_nums, times = session.get_unit_times(x)
+        clustering.set_dataset('cluster_nums', cluster_nums)
+        UnitTimes = shank.make_group('UnitTimes', attrs= {'description': 'Estimated spike times from a single unit'})
+        for i in times.keys():
+            cluster_set = UnitTimes.make_group('<unit_N>', str(i))
+            cluster_set.set_dataset('times', times[i])
+            cluster_set.set_dataset('source', 'From klustakwik, curated with Klusters')
+            cluster_set.set_dataset('unit_description', 'unit '+str(i))
     
     f.close()
     
+    print 'Finished writing .nwb file'
     session.cleanup()
     
     os.chdir(cwd)
@@ -324,10 +380,10 @@ def test_stuff(sessionName):
     os.chdir(cwd)
     
     
-test_stuff('ec013.156')
+#test_stuff('ec013.156')
 #test_stuff('ec014.468')
 
-#write_nwb('ec013.156')
+write_nwb('ec013.157')
 #write_nwb('ec014.639')
 #write_nwb('ec013.756')
 #write_nwb('750')
