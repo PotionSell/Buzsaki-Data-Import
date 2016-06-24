3a2c1c57f44b929f7a3e46a2d8c3ed0a555c254d
PotionSell <b3cousins@aol.com>
2016-06-24 17:38:40 -0400

    Add files via upload
    Backup before changing the way LFP data is handled

--- session_class.py
+++ session_class.py
@@ -169,7 +169,7 @@ class Session():
         creates a set of pos_timestamps for a given session time (times for sessions are specified in:
         /home/sidious/Desktop/docs_Bryce_s/CMB2NWBproject/NWB_sample_data/Buzsaki/Metadata/hc3-metadata-tables/hc3-session.csv
         '''
-
+    
         #an interval found in old Buzsaki-to-CMB matlab code; supposedly in https://crcns.org/files/data/hc3/crcns-hc3-data-description.pdf
         increment = 1/39.06
         
@@ -247,7 +247,7 @@ class Session():
     def load_LFPdata(self):
         '''
         Loads LFP data for all channels into numpy format.
-        '''    
+        '''
         
         os.chdir(self.sessionDir)
         
@@ -311,13 +311,16 @@ class Session():
         self.LFP_data = numpy.empty([self.all_LFP_data.shape[0], int(active_channels[-1])+1])        #assumes the same number of lines exists for all channels
         for i in active_channels:
             self.LFP_data[:, i] = self.all_LFP_data[:, i]
-
+        
         #above, 'self.LFP_data' will have indices for all active/inactive channels, but will have 0.0 for all
         #inactive entries
         
         data = self.access_shankLFP(shanks)
         self.LFP_timestamps = [x/1250. for x in range(len(data[0]))]
         print '***Done loading LFP data.***\n'
+        
+        os.chdir(cwd)
+        
         return data
         
     def access_shankLFP(self, shank):
@@ -326,11 +329,14 @@ class Session():
         Called by load_LFPdata() but can also be called by user to access specific sets.
         '''
         
+        os.chdir(self.sessionDir)
+        
         if type(shank) is int:
             curr_channels = self.active_groups_channels[shank]
             LFP_data_slice = numpy.empty([self.LFP_data.shape[0], len(curr_channels)])
             for x in range(len(curr_channels)):
                 LFP_data_slice[:, x] = self.LFP_data[:, curr_channels[x]]
+            os.chdir(cwd)
             return LFP_data_slice
         
         elif type(shank) is list:
@@ -341,6 +347,7 @@ class Session():
                 for z in range(len(curr_channels[y])):
                     LFP_data_slice[:, z] = self.LFP_data[:, curr_channels[y][z]]
                 LFP_data_slice_list.append(LFP_data_slice)
+            os.chdir(cwd)
             return LFP_data_slice_list
             
         #to save LFP data as a file: do "numpy.savetxt('name', self.access_shankLFP(X), fmt='%.10f', delimiter=',')
@@ -351,6 +358,8 @@ class Session():
         Gets LFP data in a format usable for CSD calculation.
         '''
         
+        os.chdir(self.sessionDir)
+        
         curr_channels = [self.active_groups_channels[x] for x in range(session.num_shanks)]
         for y in range(session.num_shanks):
             LFP_data_slice = numpy.empty([session.LFP_data.shape[0], len(curr_channels[y])])
@@ -360,6 +369,9 @@ class Session():
                 active_LFPdata = LFP_data_slice
             else:
                 active_LFPdata = numpy.concatenate((active_LFPdata, LFP_data_slice), axis=1)
+        
+        os.chdir(cwd)
+        
         return active_LFPdata
         
     def CSD(self, LFP):
@@ -367,11 +379,16 @@ class Session():
         Performs CSD analysis on given LFP data.
         '''
         
+        os.chdir(self.sessionDir)
+        
         #Adapted from http://fmatoolbox.sourceforge.net/API/FMAToolbox/Analyses/CSD.html
         mean = LFP.mean(axis=0)
         mean = mean.reshape(1,-1)
         LFP = LFP - numpy.tile(mean, (len(self.LFP_timestamps), 1))
         d = -numpy.diff(LFP,2,1)
+        
+        os.chdir(cwd)
+        
         return d
         
     def cleanup(self):
